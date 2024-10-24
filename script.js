6304e4114ce9669d3fe7990316d3180f5ecbab86
Ashley Fullero <146059438+AshleyFullero@users.noreply.github.com>
2024-10-24 16:22:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,41 +3,38 @@ const noBtn = document.querySelector(".no-btn");
 const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 const message = document.querySelector(".message");
-let firstClick = true; // Track if the first Yes has been clicked
+let firstClick = true; 
 
-// Change text and gif when the Yes button is clicked
+
 yesBtn.addEventListener("click", () => {
     if (firstClick) {
-        // First Yes button click
+        
         question.innerHTML = "Really? I love you too! 😘"; 
-        gif.src = "https://c.tenor.com/oZf3_XHoJ1cAAAAC/tenor.gif"; // Updated GIF URL
-        firstClick = false; // Set to false to track that the first click has happened
+        gif.src = "https://media.tenor.com/images/d5b0c8c3f29cfb2f7be2acbe6232056d/raw"; 
+        firstClick = false; 
     } else {
-        // Second Yes button click
+        
         message.innerHTML = "I don’t believe you, where is this love? HUH! Message me ASAP!";
-        message.style.display = "block"; // Show the message
+        message.style.display = "block"; 
     }
 });
 
-// Make the No button move randomly on hover
 noBtn.addEventListener("mouseover", () => {
     const wrapper = document.querySelector(".wrapper");
     const wrapperRect = wrapper.getBoundingClientRect();
     const noBtnRect = noBtn.getBoundingClientRect();
-
-    // Calculate max positions to ensure the button stays within the wrapper
+    
     const maxX = wrapperRect.width - noBtnRect.width;
     const maxY = wrapperRect.height - noBtnRect.height;
 
     const randomX = Math.floor(Math.random() * maxX);
     const randomY = Math.floor(Math.random() * maxY);
 
-    noBtn.style.position = "absolute"; // Make sure it's positioned absolutely
+    noBtn.style.position = "absolute"; 
     noBtn.style.left = randomX + "px";
     noBtn.style.top = randomY + "px";
 });
 
-// Prevent the No button from performing any action
 noBtn.addEventListener("click", (event) => {
-    event.preventDefault(); // Prevent default action
+    event.preventDefault(); 
 });
